ac4eb457d5cb136dd1cae666a7a32b55c56b0cb4
AokiTakuma <221205001@ccmailg.meijo-u.ac.jp>
2023-04-28 15:39:40 +0900

    ミノの色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'lime', 'blueviolet'
+    'cyan', 'orange', 'blue', 'yellow', 'tomato', 'lime', 'blueviolet'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
